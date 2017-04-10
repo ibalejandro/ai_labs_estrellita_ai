d2b4e0cb474f3a5f24a017624a52bde1d9719e49
ibalejandro <asanch41@eafit.edu.co>
2017-04-10 07:56:39 -0500

    Uploading the probability tables to apply forward algorithm and particle filtering.

---
+++ Agente_A_Star.py
@@ -0,0 +1,54 @@
+init_prob = 1 / 25
+one_half = 1 / 2
+one_third = 1 / 3
+one_fourth = 1 / 4
+belief_prev_t = [[init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob]]
+belief_after_t = [[init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob],
+                 [init_prob, init_prob, init_prob, init_prob, init_prob]]
+belief_after_t_and_obs = [[init_prob, init_prob, init_prob, init_prob, init_prob],
+                          [init_prob, init_prob, init_prob, init_prob, init_prob],
+                          [init_prob, init_prob, init_prob, init_prob, init_prob],
+                          [init_prob, init_prob, init_prob, init_prob, init_prob],
+                          [init_prob, init_prob, init_prob, init_prob, init_prob]]
+transition_prob = {(0, 0): {(0, 1): one_half, (1, 0): one_half},
+                   (0, 1): {(0, 0): one_third, (0, 2): one_third, (1, 1): one_third},
+                   (0, 2): {(0, 1): one_third, (0, 3): one_third, (1, 2): one_third},
+                   (0, 3): {(0, 2): one_third, (0, 4): one_third, (1, 3): one_third},
+                   (0, 4): {(0, 3): one_half, (1, 4): one_half},
+                   (1, 0): {(0, 0): one_third, (1, 1): one_third, (2, 0): one_third},
+                   (1, 1): {(0, 1): one_fourth, (1, 0): one_fourth, (1, 2): one_fourth, (2, 1): one_fourth},
+                   (1, 2): {(0, 2): one_fourth, (1, 1): one_fourth, (1, 3): one_fourth, (2, 2): one_fourth},
+                   (1, 3): {(0, 3): one_fourth, (1, 2): one_fourth, (1, 4): one_fourth, (2, 3): one_fourth},
+                   (1, 4): {(0, 4): one_third, (1, 3): one_third, (2, 4): one_third},
+                   (2, 0): {(1, 0): one_third, (2, 1): one_third, (3, 0): one_third},
+                   (2, 1): {(1, 1): one_fourth, (2, 0): one_fourth, (2, 2): one_fourth, (3, 1): one_fourth},
+                   (2, 2): {(1, 2): one_fourth, (2, 1): one_fourth, (2, 3): one_fourth, (3, 2): one_fourth},
+                   (2, 3): {(1, 3): one_fourth, (2, 2): one_fourth, (2, 4): one_fourth, (3, 3): one_fourth},
+                   (2, 4): {(1, 4): one_third, (2, 3): one_third, (3, 4): one_third},
+                   (3, 0): {(2, 0): one_third, (3, 1): one_third, (4, 0): one_third},
+                   (3, 1): {(2, 1): one_fourth, (3, 0): one_fourth, (3, 2): one_fourth, (4, 1): one_fourth},
+                   (3, 2): {(2, 2): one_fourth, (3, 1): one_fourth, (3, 3): one_fourth, (4, 2): one_fourth},
+                   (3, 3): {(2, 3): one_fourth, (3, 2): one_fourth, (3, 4): one_fourth, (4, 3): one_fourth},
+                   (3, 4): {(2, 4): one_third, (3, 3): one_third, (4, 4): one_third},
+                   (4, 0): {(3, 0): one_half, (4, 1): one_half},
+                   (4, 1): {(3, 1): one_third, (4, 0): one_third, (4, 2): one_third},
+                   (4, 2): {(3, 2): one_third, (4, 1): one_third, (4, 3): one_third},
+                   (4, 3): {(3, 3): one_third, (4, 2): one_third, (4, 4): one_third},
+                   (4, 4): {(3, 4): one_half, (4, 3): one_half}}
+sonar_prob_given_dist = {0: {"green": 0.4, "yellow": 0.3, "orange": 0.2, "red": 0.1},
+                         1: {"green": 0.3, "yellow": 0.4, "orange": 0.2, "red": 0.1},
+                         2: {"green": 0.2, "yellow": 0.5, "orange": 0.2, "red": 0.1},
+                         3: {"green": 0.15, "yellow": 0.25, "orange": 0.4, "red": 0.2},
+                         4: {"green": 0.1, "yellow": 0.2, "orange": 0.25, "red": 0.45},
+                         5: {"green": 0.4, "yellow": 0.3, "orange": 0.2, "red": 0.1},
+                         6: {"green": 0.3, "yellow": 0.4, "orange": 0.2, "red": 0.1},
+                         7: {"green": 0.2, "yellow": 0.5, "orange": 0.2, "red": 0.1},
+                         8: {"green": 0.15, "yellow": 0.25, "orange": 0.4, "red": 0.2}}
+
